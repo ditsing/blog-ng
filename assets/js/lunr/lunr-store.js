18721000afd11f7169baa154f2f7f8f8365dbc60
Jing Yang <ditsing@gmail.com>
2020-08-10 06:53:46 -0700

    First push.

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "A new Blog",
+        "excerpt":"I’m starting a new blog!   ","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2020/08/09/a-new-blog.html",
+        "teaser": null
+      }]
